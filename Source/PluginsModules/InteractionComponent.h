909e5de936e547756a5a47ea0f2dc93a86a0da56
Roman <roman.it.kirov@yandex.ru>
2022-10-20 00:11:07 +0300

    3.0.0
    Lesson 3 on the way

---
+++ Source/PluginsModules/InteractionComponent.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
+#include "InteractionComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup=(Collision),meta=(BlueprintSpawnableComponent,DisplayName="Interaction Collision"))
+class PLUGINSMODULES_API UInteractionComponent : public USphereComponent
+{
+	GENERATED_BODY()
+public:
+	UInteractionComponent();
+protected:
+	virtual void BeginPlay() override;
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UUserWidget> InteractMessageWidget;
+	
+	UPROPERTY()
+	UUserWidget* InteractMessage;
+
+	UPROPERTY()
+	AActor* ActorToInteract;
+
+	UFUNCTION()
+	void OnComponentBeginOverlapFunc(UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnComponentEndOverlapFunc(UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,UPrimitiveComponent* OtherComp,int32 OtherBodyIndex);
+
+	UFUNCTION(BlueprintCallable)
+	void Interact();
+};
